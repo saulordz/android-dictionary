86e61fe26053d04cdd2c5ef8776c1546da1fadd7
saulordz <saulordz@gmail.com>
2020-06-20 16:34:35 -0400

    refactor MaterialDialog assertions

--- app/src/test/java/com/saulordz/dictionary/testUtils/MaterialDialogAssertExtensions.kt
+++ app/src/test/java/com/saulordz/dictionary/testUtils/MaterialDialogAssertExtensions.kt
@@ -4,6 +4,10 @@ import android.widget.TextView
 import assertk.Assert
 import assertk.assertions.isEqualTo
 import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.WhichButton.NEGATIVE
+import com.afollestad.materialdialogs.WhichButton.POSITIVE
+import com.afollestad.materialdialogs.actions.getActionButton
+import com.afollestad.materialdialogs.list.getListAdapter
 import com.saulordz.dictionary.R
 
 internal fun Assert<MaterialDialog>.hasTitle(expectedText: String) = given { actual ->
@@ -11,15 +15,15 @@ internal fun Assert<MaterialDialog>.hasTitle(expectedText: String) = given { act
 }
 
 internal fun Assert<MaterialDialog>.hasPositiveText(expectedText: String) = given { actual ->
-  assertThat(actual.findViewById<TextView>(R.id.md_button_positive).text).isEqualTo(expectedText)
+  assertThat(actual.getActionButton(POSITIVE).text).isEqualTo(expectedText)
 }
 
 internal fun Assert<MaterialDialog>.hasNegativeText(expectedText: String) = given { actual ->
-  assertThat(actual.findViewById<TextView>(R.id.md_button_negative).text).isEqualTo(expectedText)
+  assertThat(actual.getActionButton(NEGATIVE).text).isEqualTo(expectedText)
 }
 
 internal fun Assert<MaterialDialog>.hasItemCount(itemCount: Int) = given { actual ->
-  assertThat(actual.view.contentLayout.recyclerView?.adapter?.itemCount).isEqualTo(itemCount)
+  assertThat(actual.getListAdapter()?.itemCount).isEqualTo(itemCount)
 }
 
 
